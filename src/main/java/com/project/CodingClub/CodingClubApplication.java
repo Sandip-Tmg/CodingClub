19b31e2bb6a749f8dd16fbf705a182621ccb4c85
enzo.corsini <enzo.corsini@bearingpoint.com>
2024-04-10 14:41:07 +0100

    main commit

---
+++ src/main/java/com/project/CodingClub/CodingClubApplication.java
@@ -0,0 +1,13 @@
+package com.project.CodingClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/CodingClub/CodingClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CodingClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
