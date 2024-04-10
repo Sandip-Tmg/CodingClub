390c7a216af2c2ef5a0fd974f4bad252dd6c0bff
enzo.corsini <enzo.corsini@bearingpoint.com>
2024-04-10 15:08:45 +0100

    added second worker and accessed gateway unhappy path

--- src/main/java/com/project/CodingClub/workers/testWorker.java
+++ src/main/java/com/project/CodingClub/workers/testWorker.java
@@ -1,5 +1,7 @@
 package com.project.CodingClub.workers;
 
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +11,8 @@ public class testWorker {
 
 
     @JobWorker(type = "validatingCard")
-    public void handleJob() {
-        System.out.println("Handling job");
+    public void handleJob(JobClient jobClient, ActivatedJob job) {
+        System.out.println("validating card");
+        jobClient.newCompleteCommand(job).variable("valid", false).send().join();
     }
 }
