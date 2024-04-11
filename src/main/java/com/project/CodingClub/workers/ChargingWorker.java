98c74bbaa4e461d8cfd288987dd8f1f929b8ce6c
enzo.corsini <enzo.corsini@bearingpoint.com>
2024-04-11 12:27:37 +0100

    applied initial logic to ChargingWorker

--- src/main/java/com/project/CodingClub/workers/ChargingWorker.java
+++ src/main/java/com/project/CodingClub/workers/ChargingWorker.java
@@ -11,6 +11,19 @@ public class ChargingWorker {
 
     @JobWorker(type = "chargeCard")
     public void chargeCard(JobClient jobClient, ActivatedJob job) {
-        System.out.println("Handling job");
+        Double amountToPay = (Double) job.getVariable("newToPay");
+        Double amountInCard = (Double) job.getVariable("balance");
+        Double newBalance = amountInCard - amountToPay;
+        if(newBalance < 0){
+            jobClient.newThrowErrorCommand(job)
+                    .errorCode("Insufficient funds")
+                    .send()
+                    .join();
+        } else {
+            jobClient.newCompleteCommand(job)
+                    .variables("{\"balance\": " + newBalance + "}")
+                    .send()
+                    .join();
+        }
     }
 }
